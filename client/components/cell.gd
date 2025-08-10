ff4703cb733860057d75a819cda287bf7c6e35a9
Kamiko Loss <info@gloxi.net>
2025-08-11 06:40:56 +0900

    Cell, CellGroup を作成した

--- client/components/cell.gd
+++ client/components/cell.gd
@@ -1,22 +1,12 @@
 class_name Cell
 extends Control
-## ドラッグする呪文の1文字分に該当するクラス
+## 呪文の文字の1マス分
 
-## ホバー時
-signal hovered
+@export var _label: Label
+@export var _bg: ColorRect
 
-@export var label: Label
-@export var bg: ColorRect
-
-@export var is_holder := false
-
-
-# ドラッグ中かどうか
-var is_draggins := false
-
-
-
-
-func _get_drag_data(at_position: Vector2) -> Variant:
-    set_drag_preview(self)
-    return self
+## 呪文の1文字
+var letter := "":
+    set(v):
+        letter = v
+        _label.text = v
---
+++ client/components/cell_group.gd
@@ -0,0 +1,70 @@
+class_name CellGroup
+extends Control
+## 呪文の文字群
+
+## Cell の一辺の長さ (px)
+const CELL_LENGTH = 40
+
+## Cell のシーン元
+@export var _cell_scene: PackedScene
+
+## 呪文の文字列
+var spell := ""
+## 形の二次元配列 (1 = セルあり, 0 = セルなし)
+## 例 (T 字型): [[0,1,0],[1,1,1]]
+var shape := []
+## ドラッグ中かどうか
+var is_dragging := false
+
+
+func _ready() -> void:
+    # debug
+    spell = "LETTERS"
+    shape = [[0,1,0],[1,1,1]]
+
+    _init_shape()
+
+
+func _notification(what: int) -> void:
+    match what:
+        # ドラッグ開始時
+        NOTIFICATION_DRAG_BEGIN:
+            modulate.a = 0.8
+        # ドラッグ終了時
+        NOTIFICATION_DRAG_END:
+            # ドラッグ失敗時
+            if not get_viewport().gui_is_drag_successful():
+                modulate.a = 1.0
+
+
+## マウスカーソルがオブジェクト内にあるかどうか
+## この結果により _get_drag_data() が発火するかどうかが決まる
+func _has_point(point: Vector2) -> bool:
+    # TODO
+    print("_has_point", point)
+    return true
+
+
+func _get_drag_data(at_position: Vector2) -> Variant:
+    print("_get_drag_data", at_position)
+    var preview := duplicate() as Control
+    set_drag_preview(preview)
+    return { "node": self }
+
+
+## 自身の形状を初期化する
+func _init_shape() -> void:
+    var index := 0
+    var cell_x := 0
+    var cell_y := 0
+    for row in shape:
+        cell_x = 0
+        for col in row:
+            if col == 1:
+                var cell := _cell_scene.instantiate()
+                cell.letter = spell[index]
+                cell.position = Vector2(cell_x, cell_y)
+                add_child(cell)
+                index += 1
+            cell_x += CELL_LENGTH
+        cell_y += CELL_LENGTH
