b4b0b99b7734caf987a04c43cb969aea4fcfe9d2
Kamiko Loss <info@gloxi.net>
2025-08-23 03:25:27 +0900

    セッション開始 API ざっくり

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,43 @@
 import { Hono } from 'hono'
+import type { CloudflareBindings } from './types'
+import type {
+  StartBattleRequest,
+  StartBattleResponse,
+  StartSessionRequest,
+  StartSessionResponse,
+} from './types/api-types'
+import { getRandomTerms, getUnixtimeDesc } from './utils'
 
-const app = new Hono()
+type Env = {
+  Bindings: CloudflareBindings
+}
+
+const app = new Hono<Env>()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
+
+/**
+ * セッション開始
+ */
+app.post('/api/start-session', async (c) => {
+  const req = await c.req.parseBody<StartSessionRequest>()
+
+  // 名辞の選択肢を抽選する
+  const randomTerms = getRandomTerms()
+
+  // セッションデータを作成する
+  const sessionKey = `${req.userId}:${getUnixtimeDesc()}`
+  c.env.SESSIONS.put(sessionKey, JSON.stringify({ termsHistory: [randomTerms] }))
+
+  // レスポンス
+  const res: StartSessionResponse = {
+    sessionId: sessionKey,
+    terms: randomTerms,
+  }
+  return c.json(res)
+})
+
 
 export default app
